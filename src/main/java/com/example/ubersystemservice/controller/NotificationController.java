f4c6eb399fe03dac40a243c3993679eaba4e400d
Vinul Vinsura <vinulviksura16@gmail.com>
2024-07-17 21:59:22 +0530

    creat NotificationController

--- src/main/java/com/example/ubersystemservice/controller/NotificationController.java
+++ src/main/java/com/example/ubersystemservice/controller/NotificationController.java
@@ -1,34 +1,9 @@
 package com.example.ubersystemservice.controller;
 
-import com.example.ubersystemservice.dto.NotificationDto;
-import com.example.ubersystemservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequiredArgsConstructor
 public class NotificationController {
-    private final NotificationService notificationService;
-
-    //The notification is posted by the system
-    @PostMapping("/post-notification")
-    public ResponseEntity<NotificationDto> postNotification(@RequestBody NotificationDto dto){
-        NotificationDto notificationDto = notificationService.postNotification(dto);
-        return ResponseEntity.ok(notificationDto);
-    }
-
-    //Retrieve all notifications for the passenger.
-    @GetMapping("/all-notifications")
-    public ResponseEntity<List<NotificationDto>> getAllNotification(){
-        List<NotificationDto> notificationDtoList = notificationService.getAllNotification();
-        return ResponseEntity.ok(notificationDtoList);
-
-    }
-
-
-
 }
